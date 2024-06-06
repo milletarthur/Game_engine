cc557c86574fbc3875f4a8d0fbb182365ee7653c
Antonia Ivanova <antoniaivanova306@gmail.com>
2024-06-06 11:11:55 +0200

    Obstacle implemented

--- Snake/src/Model_Snake/Obstacle.java
+++ Snake/src/Model_Snake/Obstacle.java
@@ -1,5 +1,26 @@
 package Model_Snake;
 
-public class Obstacle {
+public class Obstacle extends Cell {
+	
+	public Obstacle(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	void egg(int x, int y) {
+		new Obstacle(x,y);
+		
+	}
+
+	@Override
+	void kill() {
+		this.Valid = false;
+	}
+
+	@Override
+	boolean valid() {
+		return this.Valid;
+	}
 
 }

8eccf21eac1eaed5384592a220d630434dd97679
Dila Memil <dilahatunmemil@gmail.com>
2024-06-06 12:49:36 +0200

    Field_Done

--- Snake/src/Model_Snake/Field.java
+++ Snake/src/Model_Snake/Field.java
@@ -1,9 +1,35 @@
 package Model_Snake;
 
 public class Field {
-	
+	private int colonne;
+	private int ligne;
+	private Entity[][] grid;
+	private Snake snake;
+
 	Entity elementAt(int x, int y) {
-		throw new RuntimeException("Not implemented Yet !");
+		// throw new RuntimeException("Not implemented Yet !");
+		return grid[x][y];
+
+	}
+
+	public Field(int col, int lig) {
+		grid = new Entity[col][lig];
+		this.colonne = col;
+		this.ligne = lig;
+		for (int i = 0; i < lig; i++) {
+			for (int j = 0; i < col; j++) {
+				grid[i][j] = new Void(i, j);
+			}
+		}
+		grid[5][5] = new Apple(5, 5);
+	}
+
+	void c_init(int c) {
+		this.colonne = c;
+	}
+
+	void l_init(int l) {
+		this.ligne = l;
 	}
 
 }
--- Snake/src/Model_Snake/Queue.java
+++ Snake/src/Model_Snake/Queue.java
@@ -1,5 +1,16 @@
 package Model_Snake;
 
-public class Queue {
+public class Queue extends Entity {
+
+	public Queue(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	void egg(int x, int y) {
+		// TODO Auto-generated method stub
+
+	}
 
 }

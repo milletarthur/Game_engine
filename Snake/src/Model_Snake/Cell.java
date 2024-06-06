f74f8492cef2d474e2d214808f7cd6d350c3b3e2
Antonia Ivanova <antoniaivanova306@gmail.com>
2024-06-06 11:17:29 +0200

    des modifs en plus

--- Snake/src/Model_Snake/Cell.java
+++ Snake/src/Model_Snake/Cell.java
@@ -5,17 +5,15 @@ public abstract class Cell {
 	public static final int SOUTH = 2;
 	public static final int EAST = 3;
 	public static final int WEST = 4;
-	
+
 	protected int x;
 	protected int y;
-	
-	protected int Orientation = NORTH;
-	
-	protected boolean Valid = true;
-	
-	
-	abstract void egg(int x, int y);
 
+	protected int Orientation = NORTH;
+
+	protected boolean Valid = true;
+
+	abstract void egg(int x, int y);
 
 	void kill() {
 		this.Valid = false;
@@ -24,11 +22,11 @@ public abstract class Cell {
 	boolean valid() {
 		return this.Valid;
 	}
-	
+
 	int x() {
 		return x;
 	}
-	
+
 	int y() {
 		return y;
 	}

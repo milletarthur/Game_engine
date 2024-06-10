0466813897180097ae120c86f01919b07f2d306c
Dila Memil <dilahatunmemil@gmail.com>
2024-06-10 13:44:01 +0200

    Ajout structure du model

---
+++ info3.game.given.2021/src/Labyrinthe/Entity.java
@@ -0,0 +1,60 @@
+package Labyrinthe;
+
+/// Coucou : branche model
+
+
+public abstract class Entity {
+	protected Field f;
+	
+	protected int x;
+	protected int y;
+
+	protected int Orientation = Direction.N;
+
+	protected boolean Valid = true;
+	
+	protected int team;
+	
+	protected int category;
+
+	abstract void egg(int x, int y);
+
+	abstract public void move();
+
+	abstract public void pick();
+
+	abstract public void turn(int dir);
+	
+	abstract public void pop();
+	
+	abstract public void wizz();
+	
+	abstract public void wait();
+	
+	abstract public void explode();
+
+	public void kill() {
+		this.Valid = false;
+	}
+
+	public boolean valid() {
+		return this.Valid;
+	}
+
+	public int x() {
+		return x;
+	}
+
+	public int y() {
+		return y;
+	}
+	
+	public int direction() {
+		return this.Orientation; 
+	}
+	
+	public int category() {
+		return this.category;
+	}
+
+}

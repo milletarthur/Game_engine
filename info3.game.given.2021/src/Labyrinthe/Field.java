0e5746afa039fb2ab3fa03170d62a25dd2641af1
Abdel kader EL HOUSSAMI <abdel-kader.el-houssami@etu.univ-grenoble-alpes.fr>
2024-06-11 11:42:56 +0200

    Test commit

--- info3.game.given.2021/src/Labyrinthe/Field.java
+++ info3.game.given.2021/src/Labyrinthe/Field.java
@@ -26,6 +26,57 @@ public class Field {
 		int x2 = 0;
 		int y2 = 0;
 		labyrinthe[x2][y2] = new Void(x2, y2, 1, 1, this);
+		// Code test
+		// ####################################################
+		//  int r1 = rand.nextInt(1000);
+		/*int x1 = 1;
+		int y1 = 0;
+		int j = 0 ;
+		while ( j < ligne ) {
+			labyrinthe[x1][y1] = new Apple(x1, y1, 1, 1, this);
+			int eval  = rand.nextInt(2);
+			if ( eval == 1 && y1 < colonne - 1 ) {
+				y1++;
+			} else if ( eval == 0 && x1 < ligne - 1 ) {
+				x1++;
+				j++;
+			}
+		}
+		*/
+		
+		int x1 = 1;
+		int y1 = 0;
+
+		while (x1 < ligne && y1 < colonne) {
+		    labyrinthe[x1][y1] = new Apple(x1, y1, 1, 1, this);
+		    
+		    // Générez une direction aléatoire
+		    int eval = rand.nextInt(2);
+		    
+		    // Vérifiez si vous pouvez aller à droite
+		    if (eval == 1 && y1 < colonne - 1) {
+		        y1++;
+		    } 
+		    // Sinon, essayez de descendre
+		    else if (eval == 0 && x1 < ligne - 1) {
+		        x1++;
+		    }
+		    // Si vous ne pouvez ni aller à droite ni descendre, essayez l'autre direction
+		    else if (y1 >= colonne - 1 && x1 < ligne - 1) {
+		        x1++;
+		    } 
+		    else if (x1 >= ligne - 1 && y1 < colonne - 1) {
+		        y1++;
+		    }
+		    // Si les deux directions sont bloquées (au cas où), sortez de la boucle
+		    else {
+		        break;
+		    }
+		}
+
+		
+		
+		// #################################################
 		int r = rand.nextInt(1000);
 		for (int i = 0; i < r; i++) {
 			int direction = rand.nextInt(4);
@@ -81,6 +132,8 @@ public class Field {
 					ps.print(" ");
 				if (e instanceof Mur)
 					ps.print("O");
+				if (e instanceof Apple)
+					ps.print("*");
 			}
 			ps.print("\n");
 		}

e5c825e6c7c1ce4762f5d860b6e4e523400f65b9
Arthur Millet <arthur.millet@etu.univ-grenoble-alpes.fr>
2024-06-17 15:15:57 +0200

    Ajout méthodes dans ICondition

--- info3.game.given.2021/src/Automates/ICondition.java
+++ info3.game.given.2021/src/Automates/ICondition.java
@@ -1,8 +1,18 @@
 package Automates;
 
 import Labyrinthe.Entity;
+import toolkit.Direction;
+import toolkit.Categorie;
 
 public interface ICondition {
 
 	public boolean eval(Entity e);
+	
+	public void setDir(Direction dir);
+	
+	public void setCat(Categorie cat);
+	
+	public Categorie getCategorie();
+	
+	public Direction getDirection();
 }

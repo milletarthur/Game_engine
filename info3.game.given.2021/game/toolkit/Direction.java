04915a1406b759c6b68cf6e810a331f517be9f67
Arthur Millet <arthur.millet@etu.univ-grenoble-alpes.fr>
2024-06-17 11:05:34 +0200

    Début implem visitor

--- info3.game.given.2021/game/toolkit/Direction.java
+++ info3.game.given.2021/game/toolkit/Direction.java
@@ -5,8 +5,13 @@ public class Direction {
 	public static final int S = 2; // South
 	public static final int E = 3; // East
 	public static final int W = 4; // West
+	public static final int NE = 5; // North East
+	public static final int NW = 6; // North West
+	public static final int SE = 7; // South East
+	public static final int SW = 8; // South West
 	
 	public static final int H = 0; // Here
+	
 	public static final int F = -1; // Forward
 	public static final int B = -2; // Backward
 	public static final int L = -3; // Left

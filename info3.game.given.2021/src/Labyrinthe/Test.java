c7a2ba96e2c783020f5b5bc86a0728afe2e37c9b
Abdel kader EL HOUSSAMI <abdel-kader.el-houssami@etu.univ-grenoble-alpes.fr>
2024-06-19 13:25:22 +0200

    save changes

--- info3.game.given.2021/src/Labyrinthe/Test.java
+++ info3.game.given.2021/src/Labyrinthe/Test.java
@@ -6,7 +6,7 @@ import java.io.PrintStream;
 public class Test {
 	public static void main(String args[]) throws IOException {
 		System.out.println("Hello!");
-		Field f = new Field(10, 10, 50);
+		Field f = new Field(20, 20, 50);
 		f.printGame();
 	}
 }

e536d9dee246b49ec679310fb999a810ce7228cb
Dila Memil <dilahatunmemil@gmail.com>
2024-06-20 15:00:35 +0200

    done 7

--- info3.game.given.2021/src/Labyrinthe/Test.java
+++ info3.game.given.2021/src/Labyrinthe/Test.java
@@ -6,7 +6,7 @@ import java.io.PrintStream;
 public class Test {
 	public static void main(String args[]) throws IOException {
 		System.out.println("Hello!");
-		Field f = new Field(15, 15, 100, 25, 5, 5, 5, 5, 5, 25, 25, 50, 10, 2);
+		Field f = new Field(20, 25, 100, 10, 2, 2, 2, 2, 2, 25, 25, 50, 10, 2);
 		f.printGame();
 		// f.printLabyrinthe_tmp();
 	}

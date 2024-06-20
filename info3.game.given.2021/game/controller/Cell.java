14995bf39335e8c4b5320a9f195ed255b7344d51
Pierre Bernard <pierre.bernard@etu.univ-grenoble-alpes.fr>
2024-06-20 15:36:30 +0200

    next_to

--- info3.game.given.2021/game/controller/Cell.java
+++ info3.game.given.2021/game/controller/Cell.java
@@ -23,14 +23,14 @@ public class Cell implements ICondition {
 
 	@Override
 	public boolean eval(Entity e) {
-//		System.out.print("Cell");
-//		System.out.print("(");
-//		System.out.print(dir);
-//		System.out.print("/");
-//		System.out.print(cat);
-//		System.out.println(")");
+		System.out.print("Cell");
+		System.out.print("(");
+		System.out.print(dir);
+		System.out.print("/");
+		System.out.print(cat);
+		System.out.println(")");
 		Boolean t = f.cell(e,dir,cat);
-//		System.out.print(t);
+		System.out.print(t);
 		return t;
 	}
 
--- info3.game.given.2021/src/Labyrinthe/Field.java
+++ info3.game.given.2021/src/Labyrinthe/Field.java
@@ -980,6 +980,41 @@ public class Field {
 		int[] rv = new int[2];
 		rv[0] = e.ligne();
 		rv[1] = e.colonne();
+		if (d > 0) {
+			switch (d) {
+			case Direction.N:
+				rv[0]--;
+				break;
+			case Direction.S:
+				rv[0]++;
+				break;
+			case Direction.E:
+				rv[1]++;
+				break;
+			case Direction.W:
+				rv[1]--;
+				break;
+			case Direction.NE:
+				rv[0]--;
+				rv[1]++;
+				break;
+			case Direction.SE:
+				rv[0]--;
+				rv[1]++;
+				break;
+			case Direction.NW:
+				rv[0]++;
+				rv[1]--;
+				break;
+			case Direction.SW:
+				rv[0]++;
+				rv[1]--;
+				break;	
+			default :
+				break;
+			}
+			return rv;
+		}
 		if (d == Direction.H)
 			return rv;
 		switch (e.direction()) {

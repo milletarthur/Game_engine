b0c42cc5addd41704c50ccd71cbc977d5121b0de
Dila Memil <dilahatunmemil@gmail.com>
2024-06-21 17:51:26 +0200

    je sais pas

--- info3.game.given.2021/src/Labyrinthe/Test.java
+++ info3.game.given.2021/src/Labyrinthe/Test.java
@@ -10,7 +10,7 @@ public class Test {
 		/* (int lig, int col, int densite_field, int densite_pickable, int mine, int pomme, int potion, int pioche,
 		int bombe, int cassable, int invisible, int normal, int nb_porte_sable, int nb_ennemis, Random r)
 		*/
-		Field f = new Field(20, 25, 80, 5, 1, 1, 1, 1, 1, 50, 25, 25, 10, 2, new Random(0));
+		Field f = new Field(20, 25, 100, 50, 15, 15, 15, 15, 15, 50, 25, 25, 10, 2, new Random(0));
 		f.printGame();
 		// f.printLabyrinthe_tmp();
 	}

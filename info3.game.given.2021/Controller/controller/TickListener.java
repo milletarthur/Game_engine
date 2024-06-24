f9ac04b269095cda0cc1da6fc2e1e0c598ad2519
Pierre Bernard <pierre.bernard@etu.univ-grenoble-alpes.fr>
2024-06-24 11:20:27 +0200

    tl update

--- info3.game.given.2021/Controller/controller/TickListener.java
+++ info3.game.given.2021/Controller/controller/TickListener.java
@@ -1,33 +1,152 @@
 package controller;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import Automates.Automate;
 import Labyrinthe.Entity;
 import Labyrinthe.Field;
 import Labyrinthe.Joueur;
+import listener.JSONWindow;
 import toolkit.Pair;
 
-
 public class TickListener {
-	LinkedList<Pair<Automate,Entity>> auto_list;
-	
+	LinkedList<Pair<Automate, Entity>> auto_list;
+	LinkedList<Automate> all_auto_list;
+
 	public TickListener(Field terrain) {
-		auto_list = new LinkedList<Pair<Automate,Entity>>();
+		auto_list = new LinkedList<Pair<Automate, Entity>>();
 	}
-	
+
 	public void add(Automate a, Entity e) {
 		e.setCurrent(a.getInit());
-		Pair<Automate,Entity> p = new Pair<Automate,Entity>(a,e);
+		Pair<Automate, Entity> p = new Pair<Automate, Entity>(a, e);
 		auto_list.add(p);
 	}
+
+	public void setAllAutoList(LinkedList<Automate> l) {
+		all_auto_list = l;
+	}
 	
+	public void remove(Entity e) {
+		int length = auto_list.size();
+		for (int i = 0; i < length; i++) {
+			Pair<Automate, Entity> p = auto_list.get(i);
+			if (p.geto2().equals(e))
+				auto_list.remove(p);
+		}
+	}
+
+	public void add(Entity e) {
+		String classnamelong = e.getClass().getName();
+		String classname = (String) classnamelong.subSequence(classnamelong.indexOf(".") + 1, classnamelong.length());
+		int length = all_auto_list.size();
+		for (int i = 0; i < length; i++) {
+			Automate a = all_auto_list.get(i);
+			String name = a.get_name();
+			switch (classname) {
+			case "Apple":
+				if (name.equals(JSONWindow.aut_apple))
+					add(a, e);
+				break;
+			case "Arc":
+				if (name.equals(JSONWindow.aut_arc))
+					add(a, e);
+				break;
+			case "Bombe":
+				if (name.equals(JSONWindow.aut_bombe))
+					add(a, e);
+				break;
+			case "Cassable":
+				if (name.equals(JSONWindow.aut_cassable))
+					add(a, e);
+				break;
+			case "Epee":
+				if (name.equals(JSONWindow.aut_epee))
+					add(a, e);
+				break;
+			case "Fleche":
+				if (name.equals(JSONWindow.aut_fleche))
+					add(a, e);
+				break;
+			case "Interrupteur":
+				if (name.equals(JSONWindow.aut_interrupteur))
+					add(a, e);
+				break;
+			case "Invisible":
+				if (name.equals(JSONWindow.aut_invisible))
+					add(a, e);
+				break;
+			case "Joueur":
+				if (((Joueur) e).team() == 1) {
+					if (name.equals(JSONWindow.aut_j1))
+						add(a, e);
+				} else {
+					if (name.equals(JSONWindow.aut_j2))
+						add(a, e);
+				}
+				break;
+			case "Lave":
+				if (name.equals(JSONWindow.aut_lave))
+					add(a, e);
+				break;
+			case "Mine":
+				if (name.equals(JSONWindow.aut_mine))
+					add(a, e);
+				break;
+			case "Normal":
+				if (name.equals(JSONWindow.aut_normal))
+					add(a, e);
+				break;
+			case "Pioche":
+				if (name.equals(JSONWindow.aut_pioche))
+					add(a, e);
+				break;
+			case "Porte":
+				if (name.equals(JSONWindow.aut_porte))
+					add(a, e);
+				break;
+			case "Potion":
+				if (name.equals(JSONWindow.aut_potion))
+					add(a, e);
+				break;
+			case "Sable":
+				if (name.equals(JSONWindow.aut_sable))
+					add(a, e);
+				break;
+			case "Selection":
+				if (name.equals(JSONWindow.aut_selection))
+					add(a, e);
+				break;
+			case "Squelette":
+				if (name.equals(JSONWindow.aut_squelette))
+					add(a, e);
+				break;
+			case "Teleporteur":
+				if (name.equals(JSONWindow.aut_teleporteur))
+					add(a, e);
+				break;
+			case "Void":
+				if (name.equals(JSONWindow.aut_void))
+					add(a, e);
+				break;
+			case "Zombie":
+				if (name.equals(JSONWindow.aut_zombie))
+					add(a, e);
+				break;
+			default:
+			}
+		}
+	}
+
 	public void step() {
 		int length = auto_list.size();
 		for (int i = 0; i < length; i++) {
-			Pair<Automate,Entity> p = auto_list.get(i);
+			Pair<Automate, Entity> p = auto_list.get(i);
 			Automate auto = p.geto1();
 			Entity e = p.geto2();
+			if (e.getCurrent().getName().equals(""))
+				auto_list.remove(p);
 			auto.step(e);
 		}
 	}
--- info3.game.given.2021/game/controller/Wizz.java
+++ info3.game.given.2021/game/controller/Wizz.java
@@ -23,13 +23,19 @@ public class Wizz implements IAction{
 	public void exec(Entity e) {
 		if(e instanceof Cassable) {
 			terrain.remove(e.ligne(), e.colonne(), e);
-			terrain.add(new Normal(e.ligne(), e.colonne()), e.ligne(), e.colonne());
+			Normal to_add = new Normal(e.ligne(), e.colonne());
+			terrain.add(to_add, e.ligne(), e.colonne());
+			tl.add(to_add);
 		} else if(e instanceof Normal) {
 			terrain.remove(e.ligne(), e.colonne(), e);
-			terrain.add(new Invisible(e.ligne(), e.colonne()), e.ligne(), e.colonne());
+			Invisible to_add = new Invisible(e.ligne(), e.colonne());
+			terrain.add(to_add, e.ligne(), e.colonne());
+			tl.add(to_add);
 		} else if(e instanceof Invisible) {
 			terrain.remove(e.ligne(), e.colonne(), e);
-			terrain.add(new Cassable(e.ligne(), e.colonne()), e.ligne(), e.colonne());
+			Cassable to_add = new Cassable(e.ligne(), e.colonne());
+			terrain.add(to_add, e.ligne(), e.colonne());
+			tl.add(to_add);
 		} else if (e instanceof Interrupteur) {
 			LinkedList<Entity> l_levier = ((Interrupteur) e).get_entity();
 			for(int i=0; i<l_levier.size(); i++) {
@@ -45,6 +51,7 @@ public class Wizz implements IAction{
 				elem = l_entity.get(i);
 				if(elem instanceof Sable) {
 					terrain.remove(e.ligne()-1, e.colonne()-1, elem);
+					tl.remove(elem);
 				}
 			}
 			l_entity = terrain.getElement(e.ligne(), e.colonne()-1);
@@ -52,6 +59,7 @@ public class Wizz implements IAction{
 				elem = l_entity.get(i);
 				if(elem instanceof Sable) {
 					terrain.remove(e.ligne(), e.colonne()-1, elem);
+					tl.remove(elem);
 				}
 			}
 			l_entity = terrain.getElement(e.ligne()+1, e.colonne()-1);
@@ -59,6 +67,7 @@ public class Wizz implements IAction{
 				elem = l_entity.get(i);
 				if(elem instanceof Sable) {
 					terrain.remove(e.ligne()+1, e.colonne()-1, elem);
+					tl.remove(elem);
 				}
 			}
 			l_entity = terrain.getElement(e.ligne()-1, e.colonne()+1);
@@ -66,6 +75,7 @@ public class Wizz implements IAction{
 				elem = l_entity.get(i);
 				if(elem instanceof Sable) {
 					terrain.remove(e.ligne()-1, e.colonne()+1, elem);
+					tl.remove(elem);
 				}
 			}
 			l_entity = terrain.getElement(e.ligne(), e.colonne()+1);
@@ -73,6 +83,7 @@ public class Wizz implements IAction{
 				elem = l_entity.get(i);
 				if(elem instanceof Sable) {
 					terrain.remove(e.ligne(), e.colonne()+1, elem);
+					tl.remove(elem);
 				}
 			}
 			l_entity = terrain.getElement(e.ligne()+1, e.colonne()+1);
@@ -80,6 +91,7 @@ public class Wizz implements IAction{
 				elem = l_entity.get(i);
 				if(elem instanceof Sable) {
 					terrain.remove(e.ligne()+1, e.colonne()+1, elem);
+					tl.remove(elem);
 				}
 			}
 			l_entity = terrain.getElement(e.ligne()+1, e.colonne());
@@ -87,6 +99,7 @@ public class Wizz implements IAction{
 				elem = l_entity.get(i);
 				if(elem instanceof Sable) {
 					terrain.remove(e.ligne()+1, e.colonne(), elem);
+					tl.remove(elem);
 				}
 			}
 			l_entity = terrain.getElement(e.ligne()-1, e.colonne());
@@ -94,6 +107,7 @@ public class Wizz implements IAction{
 				elem = l_entity.get(i);
 				if(elem instanceof Sable) {
 					terrain.remove(e.ligne()-1, e.colonne(), elem);
+					tl.remove(elem);
 				}
 			}
 		} else if(e instanceof Pioche) {
@@ -102,6 +116,7 @@ public class Wizz implements IAction{
 				return;
 			Entity elem = terrain.getLastnotSelect(coo[0], coo[1]);
 			terrain.remove(coo[0], coo[1], elem);
+			tl.remove(elem);
 		}
 		e.wizz();
 	}

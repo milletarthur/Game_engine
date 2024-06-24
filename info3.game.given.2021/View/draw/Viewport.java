ff9b89e31180d9f4902c07bdd53b9dcaef8c6b48
Chloe Mercier <chloe.mercier@grenoble-inp.org>
2024-06-24 18:19:31 +0200

    viewport retour

--- info3.game.given.2021/View/draw/Viewport.java
+++ info3.game.given.2021/View/draw/Viewport.java
@@ -1,8 +1,6 @@
 package draw;
 
 import java.awt.*;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -13,11 +11,6 @@ public class Viewport extends JViewport {
 	private static final long serialVersionUID = 1L;
 	private DrawTerrain dt;
 	private int T_case, visibility;
-	/*private int current_x, current_y, endx, endy;
-	private long startTime;
-	private Timer timer;
-	private static final int SLIDE_DURATION = 500;
-    private static final int TIMER_DELAY = 20;*/
 
 	public Viewport(DrawTerrain dt, int T_case, int visibility) {
 		this.dt = dt;
@@ -37,33 +30,5 @@ public class Viewport extends JViewport {
 			y = 0;
 		this.setViewPosition(new Point(y, x));		
 	}
-	
-	/*public void slideTo(int targetX, int targetY) {
-        endx = targetX;
-        endy = targetY;
-        startTime = System.currentTimeMillis();
-
-        if (timer != null && timer.isRunning()) {
-            timer.stop();
-        }
-
-        timer = new Timer(TIMER_DELAY, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                long elapsedTime = System.currentTimeMillis() - startTime;
-                float progress = Math.min(1.0f, (float) elapsedTime / SLIDE_DURATION);
-
-                int currentX = (int) (current_x + progress * (endx - current_x));
-                int currentY = (int) (current_y + progress * (endy - current_y));
-
-                setViewPosition(new Point(currentY, currentX));
-
-                if (progress >= 1.0f) {
-                    timer.stop();
-                }
-            }
-        });
-        timer.start();
-    }*/
 
 }

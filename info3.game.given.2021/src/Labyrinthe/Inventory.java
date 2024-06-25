a5eefa3400eaccc5db912870e28a92780a9a3dc8
Pierre Bernard <pierre.bernard@etu.univ-grenoble-alpes.fr>
2024-06-25 09:23:37 +0200

    modifs got in hand and get

--- info3.game.given.2021/src/Labyrinthe/Inventory.java
+++ info3.game.given.2021/src/Labyrinthe/Inventory.java
@@ -83,6 +83,8 @@ public class Inventory {
 
 	public Entity popJ1() {
 		Entity e = getCurrentJ1();
+		if (e == null)
+			return null;
 		inventory.remove(e);
 		current_j1 = null;
 		current_j2 = null;
@@ -93,6 +95,8 @@ public class Inventory {
 
 	public Entity popJ2() {
 		Entity e = getCurrentJ2();
+		if (e == null)
+			return null;
 		inventory.remove(e);
 		current_j1 = null;
 		current_j2 = null;
